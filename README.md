cc91cb53cae3a9b0efceff44d1758b95baa134dd
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2022-06-13 16:36:05 +0800

    repush

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Suicidal Tweets Detector
 
+### File Structure
+
+`preprocessing.ipynb`: Data cleaning and preprocessing.
+`visualization.ipynb`: Explanatory data analysis.
+`model_training.ipynb`: Model training.
+
+`application/backend`: HTTP endpoint that serves the model.
+`application/chrome_extension`: Code for the chrome extension
--- application/backend/main.py
+++ application/backend/main.py
@@ -1,3 +1,4 @@
+import re
 from fastapi import FastAPI, status, Request
 from fastapi.middleware.cors import CORSMiddleware
 import spacy
@@ -19,6 +20,25 @@ app.add_middleware(
 )
 
 model = joblib.load("rfc.joblib")
+nlp = spacy.load('en_core_web_sm', disable = ['parser', 'ner'])
+
+def clean_text(text):
+    text = text.lower()
+    # remove @username, #hashtag, url
+    text = ' '.join(re.sub("(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ", text).split())
+    doc = nlp(text)
+
+    # lemmatization, remove stop words and other unrelevant character
+    text = ' '.join(token.lemma_ for token in doc if
+                not token.is_punct
+                and not token.is_currency
+                and not token.is_digit
+                and not token.is_space
+                and not token.is_stop
+                and not token.like_num
+                and not token.pos_ == "PROPN"
+                    )
+    return text
 
 @app.get('/', status_code=status.HTTP_200_OK)
 async def ping(request: Request):
@@ -30,6 +50,7 @@ async def ping(request: Request):
 @app.get('/predict', status_code=status.HTTP_200_OK)
 async def predict(request: Request, text: str = None):
 	if text:
+		text = clean_text(text)
 		prediction = model.predict([text])
 			
 		return {
